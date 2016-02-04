c4d6ba3c023ca74474d03271043c30c21512808f
unknown <ian.green@uk.ibm.com>
2016-02-04 15:32:19 +0000

    added typescript prep

--- packages/babel-plugin-transform-typescript-prepare/src/index.js
+++ packages/babel-plugin-transform-typescript-prepare/src/index.js
@@ -17,24 +17,67 @@ export default function ({types: t}) {
           
           application is to prep js codebase for conversion to typescript.
         */
-        VariableDeclarator: function VariableDeclarator(path,state) {
-            console.log("loc ", path.node.loc.start, ",", path.node.loc.end);
-            var id2 = t.expressionStatement(t.identifier("path.parent.type"));
-            var identifier = path.node.id;
-            var initializer = path.node.init;
-            if (null != initializer) {
-                if (initializer.type==="ObjectExpression") {
-                    // don't transform for loops.  this might be adequate
-                    if (path.parentPath.parentPath.node.type !== "ForStatement") {
-                        path.node.init = null; // remove the initializer
-                        var assignment = t.assignmentExpression("=", identifier, initializer);
-                        var stmt = t.expressionStatement(assignment);
-                        path.parentPath.insertAfter(stmt);
+        VariableDeclarator:  {
+            exit(path,state) {
+                let node = path.node;
+                // atm, always transform to typescript
+                if (true || state.opts.to_ts) {
+                    let identifier = path.node.id;
+                    let initializer = path.node.init;
+                    if (null != initializer) {
+                        if (initializer.type==="ObjectExpression"
+                           || initializer.type==="ArrayExpression")
+                        {
+                            let typedecl = t.typeAnnotation();
+                            typedecl.typeAnnotation = t.anyTypeAnnotation();
+                            path.node.id.typeAnnotation = typedecl;
+                        }
                     }
+                } else {
+                    let id2 = t.expressionStatement(t.identifier("path.parent.type"));
+                    let identifier = path.node.id;
+                    let initializer = path.node.init;
+                    if (null != initializer) {
+                        if (initializer.type==="ObjectExpression") {
+                            // don't transform for loops.  this might be adequate
+                            if (path.parentPath.parentPath.node.type !== "ForStatement") {
+                                path.node.init = null; // remove the initializer
+                                let assignment = t.assignmentExpression("=", identifier, initializer);
+                                let stmt = t.expressionStatement(assignment);
+                                path.parentPath.insertAfter(stmt);
+                            }
+                        }
+                    }
+                }
+            }
+        },
+
+        FunctionDeclaration:  {
+            exit(path,state) {
+                let node = path.node;
+                if (true || state.opts.to_ts) {
+                    let identifier = path.node.id;
+                    let initializer = path.node.init;
+                            let typedecl = t.typeAnnotation();
+                            typedecl.typeAnnotation = t.anyTypeAnnotation();
+                            path.node.returnType = typedecl;
+                }
+            }
+        },
+        FunctionExpression:  {
+            exit(path,state) {
+                let node = path.node;
+                if (true || state.opts.to_ts) {
+                    let identifier = path.node.id;
+                    let initializer = path.node.init;
+                            let typedecl = t.typeAnnotation();
+                            typedecl.typeAnnotation = t.anyTypeAnnotation();
+                            path.node.returnType = typedecl;
                 }
             }
         }
     }
   };
 }
+
 
